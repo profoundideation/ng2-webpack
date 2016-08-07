1db85d9cd0752b44f6c33ee1edf26cb2f2bb7213
Daniel Lim <hbt@markhamboards.com>
2016-08-06 22:46:33 -0400

    setup new configs
    new configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,1 @@
-entry: {
-  app: 'src/app.ts'
-}
-output: {
-  filename: 'app.js'
-}
-loaders: [
-  {
-    test: /\.ts$/
-    loaders: 'ts'
-  },
-  {
-    test: /\.css$/
-    loaders: 'style!css'
-  }
-]
+module.exports = require('./config/webpack.dev.js');
