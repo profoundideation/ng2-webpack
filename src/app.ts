42a94e94ca0b5a3a7484e4cf3523758e3142abad
Daniel Lim <hbt@markhamboards.com>
2016-08-07 00:19:47 -0400

    Keep it up
    Keep it up

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import './public/css/styles.css';
 
 @Component({
-  ...
+  selector: 'my-app',
+  templateUrl: './app/app.component.html',
+  styleUrls: ['./app/app.component.css']
 })
 
 export class AppComponent {}
