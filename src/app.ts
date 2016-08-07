e199ac89aa98cd02e4b511db4bf191b21357280b
Daniel Lim <hbt@markhamboards.com>
2016-08-06 22:41:58 -0400

    warm up
    warming up

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppComponent } from './app.component.ts';
+import 'uiframework/dist/uiframework.css';
 
 @Component({
   ...
--- webpack.config.js
+++ webpack.config.js
@@ -4,3 +4,13 @@ entry: {
 output: {
   filename: 'app.js'
 }
+loaders: [
+  {
+    test: /\.ts$/
+    loaders: 'ts'
+  },
+  {
+    test: /\.css$/
+    loaders: 'style!css'
+  }
+]
